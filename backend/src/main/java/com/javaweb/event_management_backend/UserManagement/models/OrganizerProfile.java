218f579534ef55ccba5d4a9cc0eee0778ff04971
N2K-keli <kelinguma@gmail.com>
2026-05-04 23:39:18 +0100

    FEATURE : model organizer

---
+++ backend/src/main/java/com/javaweb/event_management_backend/UserManagement/models/OrganizerProfile.java
@@ -0,0 +1,39 @@
+package com.javaweb.event_management_backend.UserManagement.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "organizer_profiles")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrganizerProfile
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(name = "organization_name", nullable = false)
+    private String organizationName;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "website")
+    private String website;
+
+    @Column(name = "logo_url")
+    private String logoUrl;
+}
